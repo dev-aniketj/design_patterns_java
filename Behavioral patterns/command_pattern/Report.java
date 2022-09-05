1e5f83de8078c4d93680595d8e31d77af7bfdee8
aniketjain8441 <aniketjain8441@gmail.com>
2022-09-05 16:28:20 +0530

    master

--- Behavioral patterns/command_pattern/Report.java
+++ Behavioral patterns/command_pattern/Report.java
@@ -1,7 +1,5 @@
 public class Report {
 
-  private String name;
-
   public void generate() {
     System.out.println("Report is being generated.");
     System.out.println("..........................");
